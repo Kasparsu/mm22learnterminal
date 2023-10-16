e25110c77557647018f1fdffe7c7ed6d57be225e
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-10-16 10:41:58 +0300

    Add yargs example

--- main.js
+++ main.js
@@ -1,39 +1,4 @@
-import inquirer from 'inquirer';
-
-function write(text) {
-    process.stdout.write(text);
-}
-
-
-inquirer
-  .prompt([
-    {type:'input', name:'name', message: 'What is your name?'},
-    {
-        type:'list',
-        name:'gender',
-        message: 'What is your gender?',
-        choices: ['male', 'female', 'bigender', 'apache helicopter', 'other']
-    },
-    {
-        type:'checkbox',
-        name:'genders',
-        message: 'What is your genders?',
-        choices: [
-            {value:'male', checked: true},
-            'female',
-            'bigender',
-            'apache helicopter',
-            'other'
-        ]
-    },
-  ])
-  .then((answers) => {
-    console.log(answers);
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-});
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+const argv = yargs(hideBin(process.argv)).argv;
+console.log(argv.value1 + ' ' + argv.sign + ' ' + argv.value2 + ' = ' );
